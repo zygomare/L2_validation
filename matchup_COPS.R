79bf6f78be3e06d12e714c3e8c0c2a8fbaafe6e8
Default Seadas User <seadas-user@localhost>
2017-03-27 09:28:00 -0400

    first_commit

---
+++ matchup_COPS.R
@@ -0,0 +1,15 @@
+library("Rimlbuoy")
+library("xts")
+library("ggplot2")
+
+
+source("~/MEGA/Arctus/project/workshop_presentation/read.extract.pix.file.R")
+load("~/MEGA/data/BoueesIML/2015/L2/COPS.DB.IML4.V3.RData")
+
+################read data 
+####################################
+#read data for viking buoy
+iml4="~/MEGA/Arctus/data/total/IML4_20160501_20161108_20170324_P3.3_RHOWN.csv"
+iml4dat=read.csv(iml4,header = T,sep = ";")
+datetime=strptime(iml4dat$TIME_IS,format = "%Y%m%dT%H%M%SZ")
+
