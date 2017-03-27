763259e1d5323c2d08713d614fba462863a1ab99
Default Seadas User <seadas-user@localhost>
2017-03-27 09:28:20 -0400

    e

--- matchup_COPS.R
+++ matchup_COPS.R
@@ -13,3 +13,7 @@ iml4="~/MEGA/Arctus/data/total/IML4_20160501_20161108_20170324_P3.3_RHOWN.csv"
 iml4dat=read.csv(iml4,header = T,sep = ";")
 datetime=strptime(iml4dat$TIME_IS,format = "%Y%m%dT%H%M%SZ")
 
+#Read modis data
+data2014.modis=read.pix_Extract("~/MEGA/Arctus/data/extracted/pixEx_modis_2014_Level 2_measurements.txt")  
+modis.day.f=as.factor(data2014.modis$data[,8])
+modis.datetime.f=strptime(paste(data2014.modis$data[,8],data2014.modis$data[,9]),format = "%Y-%m-%d %H:%M")
